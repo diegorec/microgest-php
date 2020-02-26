eaeb3ca4ce366bfb3421380fc828c170278c106e
Diego <diegogonda@recalvi.es>
2020-02-26 18:07:31 +0100

    fix error

--- librerias/neumaticos/Stock.php
+++ librerias/neumaticos/Stock.php
@@ -16,7 +16,7 @@ class Stock {
     private $ficheroDestinoStock;
     private $stockRecalvi;
     private $basedatos;
-    private $tablaStock = '';
+    private $tablaStock = 'neumaticos_soledad';
 
     public function __construct() {
         global $basesdatos;
@@ -36,7 +36,7 @@ class Stock {
     public function insertarBaseDatos(Array $stock, $config) {
         $this->basedatos->delete($this->tablaStock, []);
         foreach ($stock as $valor) {
-            $this->basedatos->insert($this->tablaStock, array(
+            $this->basedatos->debug()->insert($this->tablaStock, array(
                 'codigo' => $valor->getCodigo(),
                 'stock' => $valor->getStock(),
                 'ancho' => $valor->getAncho(),
