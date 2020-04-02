9dde2401724ddb316e8742b5c0555322c284e054
Diego <diegogonda@recalvi.es>
2020-04-02 13:07:06 +0200

    Nuevo comando para actualizar matriculas desde Microgest

---
+++ librerias/matriculas/ModificarMatriculas.php
@@ -0,0 +1,95 @@
+<?php
+
+namespace librerias\matriculas;
+
+use convertidores\CSVHandler;
+
+class ModificarMatriculas {
+
+    private $ruta;
+    private $esSuma;
+    private $centro;
+    private $db;
+    private $tabla = "users_matriculas_restantes";
+
+    public function index(Array $config) {
+        global $basesdatos;
+        if (!isset($config['suma'], $config['ruta'], $config['centro'])) {
+            throw new \Exception("No se indican los campos correctos");
+        }
+        if (!isset($basesdatos['catalogo'])) {
+            throw new \Exception("Debes configurar la base de datos del catalogo");
+        }
+        $this->centro = $config['centro'];
+        $this->esSuma = $config['suma'];
+        $this->ruta = $config['ruta'];
+        $this->db = new \Medoo\Medoo($basesdatos['catalogo']);
+
+        $this->execute();
+    }
+
+    public function execute() {
+        $csv = new CSVHandler();
+        $matriculas = $csv->_toArray($this->ruta);
+        if (!$this->esSuma) {
+            $this->insertarMatriculasGratuitasExterno($matriculas);
+        }
+        $this->actualizaMatriculasGratuitasExterno($matriculas);
+    }
+
+    public function actualizaMatriculasGratuitasExterno(Array $matriculas) {
+        foreach ($matriculas as $fila) {
+            if (is_array($fila) && isset($fila[0], $fila[1])) {
+                $nocliente = (int) $fila[0];
+                $gratuitas = (int) $fila[1];
+                $this->actualizaMatriculaGratuitaExterno($nocliente, $gratuitas);
+            }
+        }
+    }
+
+    public function actualizaMatriculaGratuitaExterno($nocliente, $gratuitas) {
+        $where = [
+            'centro' => $this->centro,
+            'nocliente' => $nocliente
+        ];
+        $current = $this->db->select($this->tabla, "*", $where);
+        if (count($current) === 0) {
+            $data = $where;
+            $data['gratuitas'] = $gratuitas;
+            $data['compradas'] = 0;
+            $data['totales_mes'] = $gratuitas;
+            $data['gratuitas_hasta'] = $this->getGratuitasHasta();
+            $this->db->insert($this->tabla, $data);
+        } else {
+            $data = $current[0];
+            $data['gratuitas'] += $gratuitas;
+            $data['totales_mes'] += $gratuitas;
+            $data['gratuitas_hasta'] = $this->getGratuitasHasta();
+            $this->db->update($this->tabla, $data, $where);
+        }
+    }
+
+    public function insertarMatriculasGratuitasExterno(Array $matriculas) {
+        foreach ($matriculas as $fila) {
+            if (is_array($fila) && isset($fila[0], $fila[1])) {
+                $nocliente = (int) $fila[0];
+                $where = [
+                    'centro' => $this->centro,
+                    'nocliente' => $nocliente
+                ];
+                $current = $this->db->select($this->tabla, "*", $where);
+                if (count($current) > 0) {
+                    $data = $current[0];
+                    $data['gratuitas'] = 0; // solo ponemos el contador a cero para pasar al proceso centralizado de actualización
+                    $data['totales_mes'] = $data['compradas'];
+                    $this->db->update($this->tabla, $data, $where);
+                }
+            }
+        }
+    }
+
+    function getGratuitasHasta() {
+        return date('Y-m-d 23:59:59', strtotime('last day of this month', time()));
+    }
+
+}
--- librerias/matriculas/include.php
+++ librerias/matriculas/include.php
@@ -3,6 +3,7 @@
 $include ['matriculas'] = array(
     'ruta' => __DIR__,
     'archivos' => array(
+        "ModificarMatriculas.php",
         'Matriculas.php')
 );
 
